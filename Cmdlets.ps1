7f7cc5c93b3fd54b7237ceca7e3436b037da1ccd
Jonas Sjömark <jonas.sjomark@kungsbacka.se>
2017-05-16 11:38:38 +0200

    Removed debug code

--- Cmdlets.ps1
+++ Cmdlets.ps1
@@ -109,7 +109,6 @@ function Send-PasswordExpirationNotice
             {
                 continue
             }
-            $x = $EmailTemplate.Replace('{NAME}', $_.GivenName).Replace('{SAM}', $_.SamAccountName).Replace('{DAYS}', $msg).Replace('{DATE}', $date)
             $mail = New-Object -TypeName 'System.Net.Mail.MailMessage'
             $mail.BodyEncoding = [System.Text.Encoding]::UTF8
             $mail.SubjectEncoding = [System.Text.Encoding]::UTF8
